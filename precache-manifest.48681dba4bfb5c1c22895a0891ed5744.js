17b18ff725091968ea29c78d8905b6654b8088f9
Milena <milena.mihneva@gmail.com>
2020-05-19 18:20:55 -0600

    Updates

---
+++ precache-manifest.48681dba4bfb5c1c22895a0891ed5744.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0429f6357dfad8948e068da10af0643c",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "3f60916e6334486687f2",
+    "url": "/reactapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "869a45158828ef6d4e3f",
+    "url": "/reactapp/static/js/2.d989f6a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp/static/js/2.d989f6a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f60916e6334486687f2",
+    "url": "/reactapp/static/js/main.e2bf6ea2.chunk.js"
+  },
+  {
+    "revision": "da8695f921a30beaffdf",
+    "url": "/reactapp/static/js/runtime-main.b8c4e6b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
